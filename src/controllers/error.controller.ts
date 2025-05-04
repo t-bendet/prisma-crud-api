8fcdc75601e9c07f13026a33cae36e23f4da635c
t-bendet <talbendet21@gmail.com>
2025-05-04 11:38:31 +0300

    feat: refactor error handling middleware and improve error response structure

--- src/controllers/error.controller.ts
+++ src/controllers/error.controller.ts
@@ -1,12 +1,12 @@
 // if error name is PrismaClientInitializationError or error instanceof PrismaClientInitializationError
 import { Request, Response, NextFunction } from "express";
-import AppError from "../utils/appError.js";
+import AppError from "../utils/appError";
 import { env } from "../utils/env";
 
-// const handleCastErrorDB = (err) => {
-//   const message = `Invalid ${err.path}: ${err.value}.`;
-//   return new AppError(message, 400);
-// };
+const handleCastErrorDB = (err: any) => {
+  const message = `${err.meta.message} - model: ${err.meta.modelName}`;
+  return new AppError(message, 400);
+};
 
 // const handleDuplicateFieldsDB = (err) => {
 //   const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
@@ -29,6 +29,7 @@ import { env } from "../utils/env";
 //   new AppError("Your token has expired! Please log in again.", 401);
 
 const sendErrorDev = (err: any, req: Request, res: Response) => {
+  // console.log(err);
   return res.status(err.statusCode).json({
     status: err.status,
     error: err,
@@ -37,43 +38,23 @@ const sendErrorDev = (err: any, req: Request, res: Response) => {
   });
 };
 
-// const sendErrorProd = (err, req, res) => {
-//   // A) API
-//   if (req.originalUrl.startsWith("/api")) {
-//     // A) Operational, trusted error: send message to client
-//     if (err.isOperational) {
-//       return res.status(err.statusCode).json({
-//         status: err.status,
-//         message: err.message,
-//       });
-//     }
-//     // B) Programming or other unknown error: don't leak error details
-//     // 1) Log error
-//     console.error("ERROR 💥", err);
-//     // 2) Send generic message
-//     return res.status(500).json({
-//       status: "error",
-//       message: "Something went very wrong!",
-//     });
-//   }
-
-//   // B) RENDERED WEBSITE
-//   // A) Operational, trusted error: send message to client
-//   if (err.isOperational) {
-//     return res.status(err.statusCode).render("error", {
-//       title: "Something went wrong!",
-//       msg: err.message,
-//     });
-//   }
-//   // B) Programming or other unknown error: don't leak error details
-//   // 1) Log error
-//   console.error("ERROR 💥", err);
-//   // 2) Send generic message
-//   return res.status(err.statusCode).render("error", {
-//     title: "Something went wrong!",
-//     msg: "Please try again later.",
-//   });
-// };
+const sendErrorProd = (err: any, req: Request, res: Response) => {
+  // A) Operational, trusted error: send message to client
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  // B) Programming or other unknown error: don't leak error details
+  // 1) Log error
+  console.error("ERROR 💥", err);
+  // 2) Send generic message
+  return res.status(500).json({
+    status: "error",
+    message: "Something went very wrong!",
+  });
+};
 
 // TODO change any?
 // TODO type tagging and predicates for the errors
@@ -100,15 +81,16 @@ export default (err: any, req: Request, res: Response, next: NextFunction) => {
   if (env.NODE_ENV === "development") {
     sendErrorDev(err, req, res);
   } else if (env.NODE_ENV === "production") {
-    // let error = { ...err };
+    let error = { ...err };
     // error.message = err.message;
-    // if (error.name === "CastError") error = handleCastErrorDB(error);
+
+    if (error.code === "P2023") error = handleCastErrorDB(error);
     // if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     // if (error.name === "ValidationError")
     //   error = handleValidationErrorDB(error);
     // if (error.name === "JsonWebTokenError") error = handleJWTError();
     // if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
-    // sendErrorProd(error, req, res);
+    sendErrorProd(error, req, res);
   }
 };
 
