4c0d665c5ea5e067c70ef5ee2f93e9fac55ac2bb
t-bendet <talbendet21@gmail.com>
2025-05-06 14:35:30 +0300

    fix: refactor user input validation and update methods in Prisma client

--- src/client.ts
+++ src/client.ts
@@ -1,12 +1,23 @@
 import { PrismaClient, Prisma } from "./generated/client"; // Adjust the import path based on your project structure
-import { z } from "zod";
+import { date, z } from "zod";
 import bcrypt from "bcrypt";
 import { env } from "./utils/env";
 
-export const UserCreateInput = z
+// TODO split extensions into separate files by model
+
+const UserUpdateInput = z
   .object({
-    name: z.string().min(1, "Name is required").max(100),
-    email: z.string().email("Please provide a valid email!"),
+    name: z.string().min(1).max(100).optional(),
+    email: z.string().email("Please provide a valid email!").optional(),
+  })
+  .strict() satisfies z.Schema<Prisma.UserUncheckedUpdateInput>; // strict mode
+
+export const UserCreateInputSchema = z
+  .object({
+    name: z.string({ message: "Name is required" }).min(1).max(100),
+    email: z
+      .string({ message: "email is required" })
+      .email("Please provide a valid email!"),
     password: z
       .string()
       .min(8)
@@ -15,13 +26,15 @@ export const UserCreateInput = z
       .string()
       .min(8)
       .max(20, "PasswordConfirm must be between 8 and 20 characters!"),
-    role: z.enum(["ADMIN", "USER"]).default("USER"), // default value
   })
   .strict() // strict mode
   .refine((data) => data.password === data.passwordConfirm, {
     message: "Password and PasswordConfirm must match!",
   }) satisfies z.Schema<Prisma.UserUncheckedCreateInput>;
 
+type UserCreateInput = z.infer<typeof UserCreateInputSchema>;
+
+// TODO  change this
 export type UserReturnType = Prisma.UserGetPayload<{
   omit: {
     password: true;
@@ -33,41 +46,16 @@ export type UserReturnType = Prisma.UserGetPayload<{
 /**
  * Prisma Client Extension
  */
-const prisma = new PrismaClient({
-  omit: {
-    user: {
-      password: true,
-      passwordConfirm: true,
-      active: true,
-    },
-  },
-  errorFormat: "colorless",
-  log: env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : [],
-}).$extends({
+const prisma = new PrismaClient().$extends({
   query: {
     user: {
       async create({ args, query }) {
-        // console.log(args.data, "args creates");
-        args.data = UserCreateInput.parse(args.data);
-        args.data.password;
-        // TODO add validation for password,will fail in bcrypt hash before validation function
-        try {
-          const hash = await bcrypt.hash(args.data["password"], 12);
-          args.data["password"] = hash;
-          args.data["passwordConfirm"] = hash;
-        } catch (error) {
-          // console.log(error);
-        }
-
+        console.log(args.data);
+        // args.data = UserCreateInput.parse(args.data);
         return query(args);
       },
       async update({ args, query }) {
-        // args.data = UserCreateInput.partial().parse(args.data);
-        // console.log(args, "args update");
-        if (typeof args.data.password === "string") {
-          args.data.password = await bcrypt.hash(args.data["password"], 12);
-        }
-
+        args.data = UserUpdateInput.parse(args.data);
         return query(args);
       },
       updateMany({ args, query }) {
@@ -83,36 +71,31 @@ const prisma = new PrismaClient({
   },
   model: {
     user: {
-      async signUp(
-        name: string,
-        email: string,
-        password: string,
-        passwordConfirm: string
-      ) {
-        const hash = await bcrypt.hash(password, 12);
+      async signUp(data: UserCreateInput) {
+        UserCreateInputSchema.parse({ ...data });
+        const hash = await bcrypt.hash(data.password, 12);
         return prisma.user.create({
           data: {
-            email,
+            ...data,
             password: hash,
             passwordConfirm: hash,
-            name,
           },
         });
       },
     },
   },
-  // result: {
-  //   user: {
-  //     madeUpField: {
-  //       needs: { name: true, role: true },
-  //       compute(user) {
-  //         console.log(user);
-  //         // the computation logic
-  //         return `${user.name} ${user.role}`;
-  //       },
-  //     },
-  //   },
-  // },
+  result: {
+    user: {
+      madeUpField: {
+        needs: { name: true, role: true },
+        compute(user) {
+          console.log(user);
+          // the computation logic
+          return `${user.name} ${user.role}`;
+        },
+      },
+    },
+  },
 });
 
 export default prisma;
