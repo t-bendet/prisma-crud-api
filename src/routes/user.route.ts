1fa90f6975bbfff1d2f7a8ecc062c9d29b909604
t-bendet <talbendet21@gmail.com>
2025-05-06 17:29:56 +0300

    minor

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -15,14 +15,22 @@ userRouter.post("/signup", validateSchema(UserCreateInputSchema), signup);
 userRouter.post("/login", validateSchema(UserTempLoginInput), login);
 userRouter.get("/logout", logout);
 
-// userRouter.route("/").get(getUsers);
+// userRouter.post('/forgotPassword', forgotPassword);
+// userRouter.patch('/resetPassword/:token', resetPassword);
 
-// userRouter.route("/:userid").get(getUser).delete(deleteUser).patch(updateUser);
+// // * USER ROUTES (protected)
+
+// userRouter.use(authenticate);
+
+// userRouter.patch('/updateMyPassword', updatePassword);
+// userRouter.get('/me', getMe);
+// userRouter.patch('/updateMe', updateMe);
+// userRouter.delete('/deleteMe', deleteMe);
+
+// // * ADMIN ROUTES (restricted to admin roles)
+
+// userRouter.use(checkAuthorization('admin'));
+// userRouter.route('/').get(getAllUsers);
+// userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
 
 export default userRouter;
-
-// TODO  validation
-// email - email shape
-//  role - enum
-//  password - min 8 characters, 1 uppercase, 1 lowercase, 1 number, 1 special character
-// confirm password - same as password
